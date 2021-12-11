b1ce9d4912ad741a17f249000b949eb161ad23fe
Ariel Conti <arielconti10@gmail.com>
2021-12-11 14:38:42 +0000

    changed the player to a lazy player to have better performance

--- src/layouts/Video/index.tsx
+++ src/layouts/Video/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import { x } from '@xstyled/styled-components'
-import YouTube from '@u-wave/react-youtube'
+import ReactPlayer from 'react-player/lazy'
 
 import BaseLayout from 'layouts/Base'
 
@@ -15,14 +15,6 @@ const VideoLayout = () => {
   const { id } = router.query
   const [{ data, isLoading }] = useVideo(id as string)
 
-  const getVideoIdFromUrl = (url: string) => {
-    const regExp =
-      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
-    const match = url.match(regExp)
-
-    return match && match[2].length === 11 ? match[2] : null
-  }
-
   return (
     <BaseLayout flexDirection="column" py={4}>
       {isLoading ? (
@@ -31,11 +23,7 @@ const VideoLayout = () => {
         <>
           <VideoCardHeader title={data?.title as string} />
           <Card w={{ xs: 'sm', md: '2xl' }} p={{ xs: 2 }}>
-            <YouTube
-              video={getVideoIdFromUrl(data?.url as string) as string}
-              width="100%"
-              height="360"
-            />
+            <ReactPlayer url={data?.url} width="100%" height="360" />
             <x.div mt={2}>
               <x.span>Video url</x.span>
             </x.div>
